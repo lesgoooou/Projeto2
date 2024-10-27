4668d5bdaf2fd272a33ac141230ddeb40d9d0fcf
BrunoBudan0 <unifbmello@fei.edu.br>
2024-10-27 15:39:12 -0300

    Tiro personagem final
    O tiro foi implementado utilizando uma thread, por meio do include Windows, que faz com que a funcionalidade do tiro trabalhe de forma independente das ações do usuário. Por causa dessa nova forma de impressão da matriz foi necessário definir uma nova organização dos inimigos, isso é feito por uma função void que organiza os inimigos na matriz de uma forma "externa" (já que antes a impressão era feita direto no 'imprimirMatriz').
    A forma de manipular os dados também foi alterada, utilizamos structs para guardar informações do personagem e dos inimigos. Essa mudança ocorreu justamente para utilizarmos diferentes formas de manipulação de variável (para encaixar o nosso código com as exigências do projeto).

--- jogo_tiro.c
+++ jogo_tiro.c
@@ -1,21 +1,46 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <conio.h>
 #include <windows.h>
 
-#define LINHAS 8
-#define COLUNAS 16
+#define LINHAS 10
+#define COLUNAS 12
 
-void imprimirMatriz(int posX, int posY, int posINIx, int posINIy, int tiroX, int tiroY) {
+typedef struct {
+    int x, y;
+} Ponto;
+
+typedef struct {
+    Ponto jogador;
+    Ponto inimigos[LINHAS][COLUNAS];
+    Ponto tiro;
+    bool tiroAtivo;
+} Estado;
+
+void inicializarInimigos(Estado *estado) {
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            estado->inimigos[i][j].x = -1;
+            estado->inimigos[i][j].y = -1;
+            if ((i == 2) && (j % 2 == 1)) { // Coloca inimigos em posições ímpares
+                estado->inimigos[i][j].x = i;
+                estado->inimigos[i][j].y = j;
+            }
+        }
+    }
+}
+
+void imprimirMatriz(Estado *estado) {
     system("cls");
     for (int i = 0; i < LINHAS; i++) {
         for (int j = 0; j < COLUNAS; j++) {
-            if (i == posX && j == posY) {
+            if (i == estado->jogador.x && j == estado->jogador.y) {
                 printf("W ");  // jogador
-            } else if (i == posINIx && j == posINIy) {
+            } else if (estado->inimigos[i][j].x != -1 && estado->inimigos[i][j].y != -1) {
                 printf("T ");  // inimigo
-                posINIy += 2;
-            } else if (i == tiroX && j == tiroY) {
+            } else if (i == estado->tiro.x && j == estado->tiro.y) {
                 printf("| ");  // tiro
             } else {
                 printf("  ");  // espaço vazio
@@ -25,49 +50,64 @@ void imprimirMatriz(int posX, int posY, int posINIx, int posINIy, int tiroX, int
     }
 }
 
-void tiro_personagem(int posX, int posY, int *tiroX, int *tiroY) {
-    if (*tiroX == -1 && *tiroY == -1) {  // Inicializa o tiro
-        *tiroX = posX - 1;
-        *tiroY = posY;
+DWORD WINAPI moverTiro(LPVOID lpParam) {
+    Estado *estado = (Estado*)lpParam;
+    while (1) {
+        if (estado->tiroAtivo) {
+            Sleep(100);
+            estado->tiro.x--;
+            if (estado->tiro.x < 0) {
+                estado->tiro.x = -1;
+                estado->tiro.y = -1;
+                estado->tiroAtivo = false;
+            }
+            imprimirMatriz(estado);
+        }
+    }
+    return 0;
+}
+
+void tiro_personagem(Estado *estado) {
+    if (!estado->tiroAtivo) {  // Inicializa o tiro
+        estado->tiro.x = estado->jogador.x - 1;
+        estado->tiro.y = estado->jogador.y;
+        estado->tiroAtivo = true;
     }
 }
 
-void abate()
+int main(void) {
+    Estado estado = {
+        .jogador = {7, 6},
+        .tiro = {-1, -1},
+        .tiroAtivo = false
+    };
 
-int main() {
-    int posX = 6, posY = 9; // Posição inicial do jogador
-    int posINIx = 2, posINIy = 1; // Posição inicial do inimigo
-    int tiroX = -1, tiroY = -1; // Posição inicial do tiro (fora da tela)
+    inicializarInimigos(&estado);
 
-    imprimirMatriz(posX, posY, posINIx, posINIy, tiroX, tiroY);
+    HANDLE hThread = CreateThread(NULL, 0, moverTiro, &estado, 0, NULL);
+
+    imprimirMatriz(&estado);
 
     while (1) {
         char tecla = _getch();
         switch (tecla) {
             case 'a': // esquerda
-                if (posY > 0) posY--;
+                if (estado.jogador.y > 0) estado.jogador.y--;
                 break;
             case 'd': // direita
-                if (posY < COLUNAS - 1) posY++;
+                if (estado.jogador.y < COLUNAS - 1) estado.jogador.y++;
                 break;
             case ' ': // tiro
-                tiro_personagem(posX, posY, &tiroX, &tiroY);
+                tiro_personagem(&estado);
                 break;
             case 'q': // sair
                 return 0;
         }
-
-        // Atualiza a posição do tiro
-        if (tiroX != -1) {
-            tiroX--;
-            if (tiroX < 0) {
-                tiroX = -1;
-                tiroY = -1;
-            }
-        }
-
-        imprimirMatriz(posX, posY, posINIx, posINIy, tiroX, tiroY);
+        imprimirMatriz(&estado);
     }
+
+    WaitForSingleObject(hThread, INFINITE); //Encerra a thraed
+    CloseHandle(hThread);
 
     return 0;
 }
